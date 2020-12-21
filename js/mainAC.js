e31b1af7658eb914c9e293de7ed63efdeeced822
luismario28 <74843140+luismario28@users.noreply.github.com>
2020-12-21 11:33:54 -0500

    boton-flotante

--- js/mainAC.js
+++ js/mainAC.js
@@ -199,3 +199,14 @@ $(".addToCart").click(function(){
 	  
 });
 
+
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
